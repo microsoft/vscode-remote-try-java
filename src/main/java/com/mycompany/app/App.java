56bcf2c52f3c1d295b114b35c9031667203ed052
m2web <m2web@yahoo.com>
2020-12-31 14:13:39 +0000

    Add test for system.out content

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Remote World!" );
+        System.out.print( "Hello Remote World!" );
     }
 }
