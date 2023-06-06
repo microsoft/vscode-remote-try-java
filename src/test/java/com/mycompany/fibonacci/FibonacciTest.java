0212c7154e4e01c7647b3d53bffe82847c9df20b
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-06-06 15:26:49 +0000

    FibonacciTest start

---
+++ src/test/java/com/mycompany/fibonacci/FibonacciTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.fibonacci;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+    int fib(int n){
+        return 0;
+    }
+    @Test
+    public void testFibonacci() {
+        assertEquals(0, fib(0));
+    }
+}
