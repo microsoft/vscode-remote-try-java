e9165c80b1f009e83313b5715e119e8adfe63b37
Vikash Kumar <vrvikashraj@gmail.com>
2023-06-30 16:55:37 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,5 +8,6 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+          System.out.println("Hello Remote World!1");
     }
 }
