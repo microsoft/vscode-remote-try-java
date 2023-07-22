14668b3bd10bd2372a8816dbd04594acac35dbbb
abdullah <abdullahfakrudeen2020@gmail.com>
2023-07-22 18:33:04 +0530

    added docker container

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,12 @@
 
 package com.mycompany.app;
 
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        
+
+        System.out.println("Hello World!!!");
+        
     }
 }
