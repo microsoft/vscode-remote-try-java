2350c3c28c19f6ea556874111f21502a41bc16b9
Carlos Felipe Ramos Pires <141787491+carlosfpr01@users.noreply.github.com>
2023-08-23 22:56:53 +0000

    coloquei o PMD pra verificação de código

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,5 +8,7 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+        System.out.println("TOMA NO CU");
     }
 }
+
